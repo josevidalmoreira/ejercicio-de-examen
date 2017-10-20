e6a8c43f9cfa17662fe73e51e8cb332ef960c15d
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2017-10-20 08:44:03 +0200

    inicio

---
+++ src/ejerciciocd/Ejerciciocd.java
@@ -0,0 +1,14 @@
+
+package ejerciciocd;
+
+/**
+ *
+ * @author jvidalmoreira
+ */
+public class Ejerciciocd {
+
+    public static void main(String[] args) {
+        System.out.println("ejercicio de examen");
+    }
+    
+}
